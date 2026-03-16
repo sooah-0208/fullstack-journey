1db3f518371716c9ae85ee6216f75a1cba6fbde3
mpsddd-commits <mpsddd@gmail.com>
2026-03-16 18:18:58 +0900

    대형비정형데이터 spark

--- practice_file/0314homework/frontend/src/pages/App.jsx
+++ practice_file/0314homework/frontend/src/pages/App.jsx
@@ -2,13 +2,11 @@ import { Routes, Route } from "react-router";
 import '@styles/App.css'
 import NotFound from '@pages/NotFound.jsx'
 import Home from '@pages/Home.jsx'
-import Home2 from '@pages/Home2.jsx'
 
 function App() {
    const paths = [
     {path: "/", element: <Home />},
     {path: "*", element: <NotFound />},
-    {path: "/home2", element: <Home2 />}
   ]
   return (
     <>
