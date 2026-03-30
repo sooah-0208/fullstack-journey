78117155ca1e3ed71b3d9ae973b14af0c92b1111
sooah-0208 <santacoyaki11@gmail.com>
2026-03-30 11:36:12 +0900

    0330

--- 연습파일/jangbaguni/frontend/src/data/data.js
+++
@@ -1,5 +0,0 @@
-export const productArr = [
-    { "no": 1, "title": "iPhone", "price": "1,700,000" },
-    { "no": 2, "title": "Galaxy", "price": "1,700,000" },
-    { "no": 3, "title": "Pixel", "price": "1,700,000" },
-  ]
--- 연습파일/jangbaguni/frontend/src/utils/network.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_APP_FASTAPI_URL || "http://localhost:8000",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
-})
